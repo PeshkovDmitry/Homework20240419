3e8ee69360ddacca2bffe08281a78b07683965dc
PeshkovDmitry <peshkov-dv@yandex.ru>
2024-04-21 09:08:21 +0300

    Добавлен сервер аутентификации и сервер ресурсов из примера на Baeldung

---
+++ auth-server/src/main/java/ru/gb/authserver/AuthServerApplication.java
@@ -0,0 +1,19 @@
+package ru.gb.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Сервер аутентификации
+ * См. https://www.baeldung.com/spring-security-oauth-auth-server
+ * Возможно он даже работает...
+ */
+
+@SpringBootApplication
+public class AuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerApplication.class, args);
+	}
+
+}
